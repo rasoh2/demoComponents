983b551e7d267378500e537e630e216ddad1708a
manicho <manicho@gmail.com>
2024-09-08 10:28:15 -0300

    first challenge

---
+++ src/components/MultipleSelect/MultipleSelect.jsx
@@ -0,0 +1,11 @@
+// La principal idea es que una persona pueda seleccionar su comuna.
+// Para eso se deben lograr los siguientes requerimientos.
+
+// - Mostrar 3 Select con Región, provincia y comuna respectivamente y un botón reset.
+// - Los select parten deshabilitados excepto el de región.
+// - Los select se irán deshabilitando a medida que se vaya seleccionando correctamente el anterior.
+// - No se puede seleccionar comuna sin antes haber seleccionado provincia.
+// - No se puede seleccionar provincia sin antes haber seleccionado región.
+// - Si se encuentran los 3 select correctamente, el botón reset se podrá seleccionar y volverá a su estado inicial el componente.
+// - El archivo con los datos se encuentra en src/assets/regiones-provincias-comunas.json
+export const MultipleSelect = () => <div>MultipleSelect</div>;
